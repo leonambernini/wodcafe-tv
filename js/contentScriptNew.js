cc144d95899d780da44634af68c2eff34e0c5027
Leonam Bernini <leonam.b@hotmail.com>
2023-12-02 13:39:07 -0300

    teste

--- js/contentScriptNew.js
+++ js/contentScriptNew.js
@@ -204,11 +204,27 @@ function checkOptionsWods(){
 }
 
 function eventos(){
-    $(document).on('DOMNodeInserted DOMNodeRemoved', '#scheduleId', function() {
+    // Função para manipular as alterações no DOM
+    function handleDOMChanges() {
         atualizaAulas();
         atualizaQtdCheckins();
         checkOptionsWods();
-    });
+    }
+
+    // Seleciona o elemento alvo
+    var targetElement = document.getElementById('scheduleId');
+
+    // Cria uma instância de MutationObserver com a função de retorno de chamada
+    var observer = new MutationObserver(handleDOMChanges);
+
+    // Configuração do observador para observar mudanças nos nós filho e remoção de nós
+    var config = { childList: true, subtree: true };
+
+    // Registra o observador no elemento alvo com a configuração
+    observer.observe(targetElement, config);
+
+    // Lembre-se de chamar observer.disconnect() quando você não precisar mais observar as mudanças no DOM para liberar recursos.
+
 
     document.addEventListener('keydown', function(e) {
         var $items = $('.lb-info-wod-item');
